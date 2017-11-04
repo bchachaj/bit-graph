e1a018d36694f92a3ea6e02fd8e7acc3c9a8c176
Ben Chachaj <benedict.chachaj@student.fairfield.edu>
2017-11-04 13:21:28 -0400

    set up project core packages

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var path = require("path");
+const webpack = require('webpack');
+
+// var plugins = [];
+// var devPlugins = [];
+
+// var prodPlugins = [
+//   new webpack.DefinePlugin({
+//     'process.env': {
+//       'NODE_ENV': JSON.stringify('production')
+//     }
+//   }),
+//   new webpack.optimize.UglifyJsPlugin({
+//     compress: {
+//       warnings: true
+//     }
+//   })
+// ];
+//
+// plugins = plugins.concat(
+//   process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+// );
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/index.jsx",
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
